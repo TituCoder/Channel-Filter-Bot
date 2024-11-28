3004d31b9193cafa21ea2e56882309444e71a630
TituCoder <asatkarsarvesh39+t2@gmail.com>
2024-11-28 17:55:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ API_ID       = 28502424
 API_HASH     = "91f949750d97b60cb424f9d7e0b95f09"
 BOT_TOKEN    = "8184797331:AAFEnRy8HxBvxVmquBP9jxC91BrtvwFE71w"
 SESSION      = "BQGy6ZgAhcj92b3h2JNtMT1Xz28BtyUvUbhCEvSg3vzFCLk_pOALG2emCPEAsk0fN6gorVYvSA-Su3zOP9sqtwPXIrP6pDo0_P0O4dDPmY-LZKy5O1ewQ3wO3fXT-HUWGESbgZ-Rbl0FZsKS7LOL-VPy2qV4OKAvjlRQzPbsWpaDGkGzI0nn5D-jvIU3Miki4EiHxjn3xQ3pXQde1f20MwqgD3ReBIhiuezApGAwwfZtZZOyAfI4MyCuPCdVHqRBy9G-w6M4kAHp7QJRnTgty6fcR3nhRndearxI_vHgGmZ2utndpC6Rnlzh1r8tMj5QbG2CewHSipTfOcC0ZLyKIuJ-FWdEGQAAAAHSIt28AA"
-DATABASE_URI = "mongodb+srv://titu:<db_password>@cluster0.c95k5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_URI = "mongodb+srv://titu:titu@cluster0.c95k5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = -1002447325061
 ADMIN        = 7820467644
 CHANNEL      = "@TituMovie1x"
